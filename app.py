888e1fcd6f43989606ce954d2447274e38ce909c
KarlapudiGopinadh <gn.kgn2323@gmail.com>
2023-06-02 07:47:51 +0530

    web app is ready

--- app.py
+++ app.py
@@ -24,5 +24,14 @@ def predict_api():
     print(output[0]) #output gives 2d array but we only need first value 
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()] #converting form values into float
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="the house price prediction is {}".format(output))
+
+
 if __name__=="__main__":
     app.run(debug=True)
